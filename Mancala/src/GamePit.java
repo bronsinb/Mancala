744327f9c368b3f96ef0b091283d96c5bc864f6d
Bronsin Benyamin <benyaminbronsin@gmail.com>
2017-10-25 21:43:59 -0700

    Adding source file

---
+++ Mancala/src/GamePit.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics2D;
+
+/**
+ * Class that draws Game pits
+ * @author Bronsin Benyamin Pouran
+ *
+ */
+public class GamePit implements Pit{
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Mancala/src/Pit.java
@@ -0,0 +1,10 @@
+import java.awt.Graphics2D;
+
+/**
+ * Interface for pits
+ * @author Bronsin Benyamin
+ *
+ */
+public interface Pit {
+	void draw(Graphics2D g);
+}
