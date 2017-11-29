79066cb4b55a228d4d3aa602d1e595e57762024f
Tai <taidao90@gmail.com>
2017-11-29 12:32:28 -0800

    adding actionListeners to buttons.

--- Mancala/src/GameFrame.java
+++ Mancala/src/GameFrame.java
@@ -1,9 +1,13 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.*;
 
 import javax.swing.*;
 
 public class GameFrame extends JFrame{
+	private Model model = new Model(4); //Creating Model here.... Temporarily set to 4 stones....
+	
 	private GamePit[] pits;
 	private PlayerPit[] playerPits;
 	private JPanel grid;
@@ -34,8 +38,20 @@ public class GameFrame extends JFrame{
 	
 		for(int i = 0; i < pits.length; i++) {
 			if(i < 6) {
+				int b = i; // clone of i for anonymous class
+				
 				pits[i] = new GamePit(stoneAmount, "A", 50);
 				style.styleGamePits(pits[i]);
+				
+				// Add action listeners to every single pit button.
+				pits[i].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						model.checkCorrectPitSelected(b);
+					}
+				});
+				
 			}
 			else {
 				pits[i] = new GamePit(stoneAmount, "B", 50);
